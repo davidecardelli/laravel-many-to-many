80805c06cb428fdb754d6f95ba5b269f99b521e7
davidecardelli <davide.cardelli.94@gmail.com>
2023-03-13 13:24:20 +0100

    add Technology migration

--- database/migrations/2023_03_13_121704_create_technologies_table.php
+++ database/migrations/2023_03_13_121704_create_technologies_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
+            $table->string('type')->unique();
+            $table->string('logo')->nullable();
             $table->timestamps();
         });
     }
